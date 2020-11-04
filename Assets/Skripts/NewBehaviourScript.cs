6d44749fa799d36f0c090794bd18fdc1b0e58a97
samost <you@example.com>
2020-11-04 21:40:29 +0300

    second commit

---
+++ Assets/Skripts/NewBehaviourScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public float speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+//        BoxCollider My = transform.GetComponent<BoxCollider>();
+//        My.enabled = false;
+
+        for (int i = 0; i < 4; i++)
+        {
+            Foo();
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Foo()
+    {
+        Vector3 mov = Vector3.left;
+
+        Rigidbody rgBdy = transform.GetComponent<Rigidbody>();
+        rgBdy.AddForce(mov*speed);
+    }
+}
